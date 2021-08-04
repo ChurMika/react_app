baa532708253ee64e34fe1a5092a780fe9cae444
Marina <churmika@gmail.com>
2021-08-05 00:39:33 +0300

    add thunk, persist

--- src/store.js
+++ src/store.js
@@ -1,8 +1,15 @@
-import { combineReducers, createStore } from 'redux'
+import { applyMiddleware, combineReducers, createStore, compose } from 'redux'
+import thunk from 'redux-thunk'
+import { persistReducer, persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import profileReducer from './components/store/Profile/reducer'
 import chatReducer from './components/store/Chat/reducer'
 import messageReducer from './components/store/Message/reducer'
 
+const persistConfig = {
+    key: 'root',
+    storage,
+}
 
 const rootReducer = combineReducers({
     profile: profileReducer,
@@ -10,7 +17,13 @@ const rootReducer = combineReducers({
     message: messageReducer
 })
 
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
 export const store = createStore(
     rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeEnhancers(applyMiddleware(thunk))
 )
+
+export const persistor = persistStore(store)
