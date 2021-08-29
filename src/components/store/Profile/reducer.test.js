aac0e14badd35dfab3f8b09e30c9381baf42decf
Marina <churmika@gmail.com>
2021-08-30 01:12:19 +0300

    profile reducer tests + loghthouse report

--- src/components/store/Profile/reducer.test.js
+++ src/components/store/Profile/reducer.test.js
@@ -1,14 +1,61 @@
 import '@testing-library/jest-dom'
 import '@testing-library/react'
-import reducer from "./reducer";
+import reducer from "./reducer"
+import { CHANGE_IS_ONLINE, CHANGE_NAME } from './actions'
 
-describe('Initial State', () => {
-    it('IS', () => {(reducer(undefined, {})).toEqual([
-      {
-        name: 'Marina',
-        age: 27,
-        isOnline: true,
-        isAuthed: false,
-      }
-    ])})
-  })
+test('should return the initial state', () => {
+  expect(reducer(undefined, {})).toEqual(
+    {
+      name: 'Marina',
+      age: 27,
+      isOnline: true,
+      isAuthed: false
+    }
+  )
+})
+
+test('should change name', () => {
+  const previousState = {
+    name: 'Marina',
+    age: 27,
+    isOnline: true,
+    isAuthed: false
+  }
+  expect(reducer(previousState, (CHANGE_NAME: {name: 'Ira'}))).toEqual(
+    {
+      name: 'Marina',
+      age: 27,
+      isOnline: true,
+      isAuthed: false
+    },
+    {
+      name: 'Ira',
+      age: 27,
+      isOnline: true,
+      isAuthed: false
+    }
+  )
+})
+
+test('should change online state', () => {
+  const previousState = {
+    name: 'Marina',
+    age: 27,
+    isOnline: true,
+    isAuthed: false
+  }
+  expect(reducer(previousState, (CHANGE_IS_ONLINE: {isOnline: false}))).toEqual(
+    {
+      name: 'Marina',
+      age: 27,
+      isOnline: true,
+      isAuthed: false
+    },
+    {
+      name: 'Marina',
+      age: 27,
+      isOnline: false,
+      isAuthed: false
+    }
+  )
+})
